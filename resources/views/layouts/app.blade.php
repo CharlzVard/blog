1c377ef3a5b179490fd75ec5e66a43707b32b89b
Charlz Vard <i@vard.su>
2018-09-20 10:22:25 +0300

    upd app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,80 +22,65 @@
 	
 	<!-- Bootstrap CSS & Font Awesome JS -->
 	<link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}" crossorigin="anonymous">
-	<link rel="stylesheet" href="{{ asset('/css/select2.min.css') }}" crossorigin="anonymous">
-	<script defer src="{{ asset('/js/fontawesome.min.js') }}" crossorigin="anonymous"></script>
-	<!-- User style -->
-	<link rel="stylesheet" href="{{ asset('/css/redactor.min.css') }}" crossorigin="anonymous">
-	<link rel="stylesheet" href="{{ asset('/css/filemanager.min.css') }}" crossorigin="anonymous">
-	
-	<link rel="stylesheet" href="{{ asset('/css/dashboard.css') }}" crossorigin="anonymous">
+	{{-- <script defer src="{{ asset('/js/fontawesome.min.js') }}" crossorigin="anonymous"></script> --}}
+	<!-- User style -->	
+	<link rel="stylesheet" href="{{ asset('/css/style.css') }}" crossorigin="anonymous">
 	
 	<title>@yield('title','Блог товарища C.V., в котором он хранит свои мысли')</title>
 </head>
 <body>
 	
 	<div id="app">
-		<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-			<div class="container">
-				<a class="navbar-brand" href="{{ url('/') }}">
-					{{ config('app.name', 'Laravel') }}
-				</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<!-- Left Side Of Navbar -->
-					<ul class="navbar-nav mr-auto">
-						<li class="nav-item">
-							<a class="nav-link{{{ Request::is('/') ? ' active' : '' }}}" href="{{ url('/') }}">Главная</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link{{{ Request::is('article*') ? ' active' : '' }}}" href="{{ route('articles') }}">Статьи</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link{{{ Route::is('about') ? ' active' : '' }}}" href="{{ route('about') }}">О проекте</a>
-						</li>
-						
-					</ul>
-					
-					<!-- Right Side Of Navbar -->
-					<ul class="navbar-nav ml-auto">
-						<!-- Authentication Links -->
-						@guest
-						<li class="nav-item">
-							<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-						</li>
-						{{-- <li class="nav-item">
-							<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-						</li> --}}
-						@else
-						<li class="nav-item dropdown">
-							<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-								{{ Auth::user()->name }} <span class="caret"></span>
-							</a>
-							
-							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-								
-								<a class="dropdown-item" href="{{ route('admin.index') }}">AsAdmin</a>
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="{{ route('logout') }}"
-								onclick="event.preventDefault();
-								document.getElementById('logout-form').submit();">
-								{{ __('Logout') }}
-							</a>
-							
-							<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-								@csrf
-							</form>
-						</div>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item{{{ Request::is('/') ? ' active' : '' }}}">
+						<a class="nav-link" href="{{ url('/') }}">Главная</a>
 					</li>
-					@endguest
+					<li class="nav-item{{{ Request::is('article*') ? ' active' : '' }}}">
+						<a class="nav-link" href="{{ route('articles') }}">Статьи</a>
+					</li>
+					<li class="nav-item{{{ Route::is('about') ? ' active' : '' }}}">
+						<a class="nav-link" href="{{ route('about') }}">О проекте</a>
+					</li>
 				</ul>
-			</div>
+				<ul class="navbar-nav mr-right">
+					@guest
+					<li class="nav-item{{{ Route::is('about') ? ' active' : '' }}}">
+						<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+					</li>
+					{{-- <li class="nav-item">
+						<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+					</li> --}}
+					@else
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							{{ Auth::user()->name }}
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="{{ route('admin.index') }}">AsAdmin</a>
+							<a class="dropdown-item" href="#">Another action</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="{{ route('logout') }}"
+							onclick="event.preventDefault();
+							document.getElementById('logout-form').submit();">
+							{{ __('Logout') }}
+						</a>
+						<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+							@csrf
+						</form>
+					</div>
+				</li>
+				@endguest
+			</ul>
+			
 		</div>
 	</nav>
-	
 	<main class="py-4">
 		@yield('content')
 	</main>
