2060fa594b12d4bc39f3326254fa7b84622c900c
Charlz Vard <i@vard.su>
2018-09-21 10:55:54 +0300

    ya metric

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,5 +91,6 @@
 <script src="{{ asset('/js/jquery-3.3.1.min.js') }}" crossorigin="anonymous"></script>
 <script src="{{ asset('/js/popper.min.js') }}" crossorigin="anonymous"></script>
 <script src="{{ asset('/js/bootstrap.min.js') }}" crossorigin="anonymous"></script>
+<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter50432263 = new Ya.Metrika2({ id:50432263, clickmap:true, trackLinks:true, accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/tag.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks2"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/50432263" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 </body>
 </html>
